09f0ad4645151298311bdae7645fbf3dd8ab8602
Pavel Pyshinskiy <p.pyshinskiy@gmail.com>
2021-03-20 18:39:56 +0700

    Project was initialized

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "ru.pyshsoft.java.exec.api"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
